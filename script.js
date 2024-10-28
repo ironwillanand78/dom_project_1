11707e126a5810b05c0627b2efe2c10f0926cfb2
AnandOjha123 <david12345hussy@gmail.com>
2024-10-28 15:12:04 +0530

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+let div = document.querySelector("#first");
+div.style.height = "210px" ;
+div.style.width = "300px" ;
+div.style.backgroundColor = "yellow";
+div.style.color = "blue";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontWeight = "900";;
+div.innerText = "Hello, I'm a div!"
+console.log(div);
